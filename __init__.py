682470f36145e7e807615b7be58e9cc8973876ce
Ahmet Alper <ahmetalpererdogan@gmail.com>
2024-08-25 23:51:17 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from .nodes import Node_1
+
+NODE_CLASS_MAPPINGS = {'Node_1' : Node_1}
+
+NODE_DISPLAY_NAME_MAPPINGS = {'Node_1' : 'Send Image to Discord Channel'}
+
+print('\033[35m')
+
+print('#----------#----------#----------#----------#----------#----------#')
+
+print('\n[ ahmetalperx ] | Custom nodes loaded.\n')
+
+print('#----------#----------#----------#----------#----------#----------#')
+
+print('\033[0m')
