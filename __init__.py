f04e8afb65adcbfbfe4bd3b3c61b188268822cbd
Ahmet Alper <ahmetalpererdogan@gmail.com>
2024-08-27 17:06:38 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 from .nodes import Node_1
 
-NODE_CLASS_MAPPINGS = {'Node_1' : Node_1}
+NODE_CLASS_MAPPINGS = {'send image to discord' : Node_1}
 
-NODE_DISPLAY_NAME_MAPPINGS = {'Node_1' : 'send image to discord channel'}
+__all__ = ['NODE_CLASS_MAPPINGS']
 
 print('\033[35m')
 
 print('#----------#----------#----------#----------#----------#----------#')
 
-print('\n[ ahmetalperx ] | Custom nodes loaded.\n')
+print('\n[ ahmetalper ] | Custom nodes loaded.\n')
 
 print('#----------#----------#----------#----------#----------#----------#')
 
